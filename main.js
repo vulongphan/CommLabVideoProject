bfe8e49b8878a7d5b4c6901d8282409a8ec38217
Long Phan <pvlong2000@gmail.com>
2021-06-30 00:09:38 +0400

    implement progress bar and refactor js code

--- main.js
+++ main.js
@@ -1,39 +1,57 @@
-var PagesArray = document.getElementsByClassName("page")
+var PagesArray = document.getElementsByClassName("page");
+var ProgressSegmentsArray = document.getElementsByClassName("progress-segment");
 var curPage = 0;
 
 
 var left_arrow = document.getElementById("left");
 var right_arrow = document.getElementById("right");
 
+// initial rendering
+PagesArray[0].style.display = 'block';
+ProgressSegmentsArray[0].style.background = "black";
+
+// display current page based on curPage index
+function displayCurrentPage(curPage) {
+    for (let i = 0; i < PagesArray.length; i++) {
+        let page = PagesArray[i];
+        if (i !== curPage) {
+            page.style.display = 'none';
+        }
+        else {
+            page.style.display = 'block';
+        }
+    }
+}
+
+// display current progress segment based on curPage index 
+function displayCurrentProgressSegment(curPage) {
+    for (let i = 0; i < ProgressSegmentsArray.length; i++){
+        let progressSegment = ProgressSegmentsArray[i];
+        if (i !== curPage) {
+            progressSegment.style.background = "white";
+        }
+        else {
+            progressSegment.style.background = "black";
+        }
+    }
+}
+
+
+// update current page index when user clicks left arrow
 left_arrow.addEventListener('click', () => {
     if (curPage > 0) {
         curPage -= 1;
-        for (let i = 0; i < PagesArray.length; i++) {
-            if (i !== curPage) {
-                let page = PagesArray[i];
-                page.style.display = 'none';
-            }
-            else {
-                let page = PagesArray[i];
-                page.style.display = 'block';
-            }
-        }
+        displayCurrentPage(curPage);
+        displayCurrentProgressSegment(curPage);
     }
 });
 
+// update current page index when user clicks right arrow
 right_arrow.addEventListener('click', ()=>{
     if (curPage < PagesArray.length-1) {
         curPage += 1
-        for (let i = 0; i < PagesArray.length; i++) {
-            if (i !== curPage) {
-                console.log("Page index to be hidden: ", curPage);
-                let page = PagesArray[i];
-                page.style.display = 'none';
-            }
-            else {
-                let page = PagesArray[i];
-                page.style.display = 'block';
-            }
-        }
+        displayCurrentPage(curPage);
+        displayCurrentProgressSegment(curPage);
     }
 })
+
